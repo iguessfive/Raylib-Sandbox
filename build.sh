43ab302c5a678b4c6eacd106f567cc0a5ba85fdc
iguessfive <iguessfive@outlook.com>
2025-06-13 17:02:36 +0100

    Setup a raylib dev environment

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-clang++ src/main.cpp -o celesteclone.exe
+clang++ src/main.cpp -o celesteclone \
+-I/opt/homebrew/include \
+-L/opt/homebrew/lib -lraylib \
+-framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
+#include "raylib.h"
 
-static bool running = true;
+int create_window(void);
 
 int main () 
 {
-    while(running)
+    create_window();
+}
+
+int create_window(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
     {
-        // update
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
     }
+
+    CloseWindow();
+
     return 0;
 }
