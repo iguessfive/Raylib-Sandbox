11005b060e5704092572b9904275ab4715707e7a
iguessfive <iguessfive@outlook.com>
2025-06-15 09:48:53 +0100

    feat: add Player object to main

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ BUILD_TYPE=${1:-release}
 
 if [[ "$BUILD_TYPE" == "debug" ]]; then
   echo "Building debug version..."
-  clang++ -g -DDEBUG src/main.cpp -o celesteclone \
+  clang++ -g -DDEBUG src/main.cpp -o raylib \
     -I/opt/homebrew/include \
     -L/opt/homebrew/lib -lraylib \
     -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
 else
   echo "Building release version..."
-  clang++ -O3 -DNDEBUG src/main.cpp -o celesteclone \
+  clang++ -O3 -DNDEBUG src/main.cpp -o raylib \
     -I/opt/homebrew/include \
     -L/opt/homebrew/lib -lraylib \
     -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,19 @@
 
 #include "debug.h"
 
+struct Player
+{
+    float speed = 120.0;
+    Vector2 global_position = {0, 0};
+    Vector2 size_rect = {100.0, 100.0};
+    Vector2 position;
+};
+
 int create_window(void);
-void handle_input(Vector2 &position, float delta);
+void handle_input(Player &player, float delta);
 
 int screen_width = 1152;
 int screen_height = 648;
-
-float speed = 120.0;
 
 int main () 
 {
@@ -17,45 +23,45 @@ int main ()
     create_window();
 }
 
-void handle_input(Vector2 &position, float delta)
+void handle_input(Player &player, float delta)
 {
     bool left = IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT);
     bool right = IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT);
 
     if (left && !right) {
-        position.x -= speed * delta;
+        player.position.x -= player.speed * delta;
     } else if (right && !left) {
-        position.x += speed * delta;
+        player.position.x += player.speed * delta;
     }
-}
+};
 
 int create_window(void)
 {
     InitWindow(screen_width, screen_height, "Celeste Clone");
 
-    //START setup for rendering character texture
-    Texture2D character = LoadTexture("character/idle.png");
-
-    int frame_width = character.width;
-    int frame_height = character.height;
-
-    Rectangle source_img = {0.0f, 0.0f, (float)frame_width, (float)frame_height};
-    Vector2 position = {(float)screen_width/2.0f, (float)screen_height/2.0f};
-    //END
+    //region: player setup 
+    Player player;
+    player.global_position = Vector2{(float)screen_width/2.0f, (float)screen_height/2.0f};
+    player.position = {
+        (player.global_position.x + player.size_rect.x) / 2.0f,
+        (player.global_position.y + player.size_rect.y) / 2.0f
+    };
+    //endregion
 
     while (!WindowShouldClose()) // main loop
     {
         float delta = GetFrameTime();
-
-        handle_input(position, delta);
+        handle_input(player, delta);
         
         BeginDrawing();
             ClearBackground(DARKGRAY);
-            DrawTextureRec(character, source_img, position, WHITE); // render texture
+            DrawRectangle(
+                player.global_position.x, player.global_position.y, player.size_rect.x, player.size_rect.y, WHITE
+            ); // render player shape
+            DrawCircle(player.position.x, player.position.y, 10.0, BLACK);
+            DrawCircle(player.global_position.x, player.global_position.y, 5.0, GOLD);
         EndDrawing();
-    }
-    
-    UnloadTexture(character);
+    };
 
     CloseWindow();
 
